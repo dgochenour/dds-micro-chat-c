5d7afec072843c121bc8b7da8d4c5637c52c2d7c
Don Gochenour <don@rti.com>
2023-02-08 09:38:11 -0500

    move includes to common header

---
+++ chat_client.h
@@ -0,0 +1,16 @@
+#ifndef CHAT_CLIENT_H
+#define CHAT_CLIENT_H
+
+// headers from Connext DDS Micro installation
+#include "rti_me_c.h"
+#include "disc_dpse/disc_dpse_dpsediscovery.h"
+#include "wh_sm/wh_sm_history.h"
+#include "rh_sm/rh_sm_history.h"
+#include "netio/netio_udp.h"
+
+// rtiddsgen generated headers
+#include "./generated/chat.h"
+#include "./generated/chatPlugin.h"
+#include "./generated/chatSupport.h"
+
+#endif
