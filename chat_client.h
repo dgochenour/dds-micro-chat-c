aba6ee48406629933d50fa2eee55a287c3b4b0ea
Don Gochenour <don@rti.com>
2023-02-10 15:17:26 -0500

    move domain_id and initial_peer to this header

--- chat_client.h
+++ chat_client.h
@@ -1,16 +1,20 @@
 #ifndef CHAT_CLIENT_H
 #define CHAT_CLIENT_H
 
-// headers from Connext DDS Micro installation
+/* headers from Connext Cert installation */
 #include "rti_me_c.h"
 #include "disc_dpse/disc_dpse_dpsediscovery.h"
 #include "wh_sm/wh_sm_history.h"
 #include "rh_sm/rh_sm_history.h"
 #include "netio/netio_udp.h"
 
-// rtiddsgen generated headers
+/* rtiddsgen generated headers */
 #include "./generated/chat.h"
 #include "./generated/chatPlugin.h"
 #include "./generated/chatSupport.h"
+
+/* user-configurable values */
+int domain_id = 100;
+char *peer = "127.0.0.1";
 
 #endif
